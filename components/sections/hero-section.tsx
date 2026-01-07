be1a568983ab0c56baf630e63ab14d752fc02848
Ethan Borkosky <ethan.borkosky@gmail.com>
2026-01-06 22:36:44 -0500

    add resume

--- components/sections/hero-section.tsx
+++ components/sections/hero-section.tsx
@@ -43,7 +43,7 @@ export function HeroSection() {
           </Button>
 
           <Button variant="outline" size="lg" asChild>
-            <a href="/resume.pdf" target="_blank">
+            <a href="/Ethan-Borkosky-Resume.pdf" target="_blank">
               Resume
             </a>
           </Button>
